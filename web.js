cb5f5560d7f10c138fe2b1fa65e1a793e4486e17
Andy Hume <andyhume@gmail.com>
2012-08-03 20:01:15 +0100

    Initial probe code.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var probe = require('./probe.js');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+  console.log(probe);
+  probe.makeRequest();
+  probe.testBrowser();
 });
 
 var port = process.env.PORT || 5000;
