82c5442f6c3bf05500c3b6ab67c78ac24d3cd6fc
Andy Hume <andyhume@gmail.com>
2012-08-11 00:34:55 +0100

    Initial test release.

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.post('/', routes.runProbeHtml);
+app.get('/probeUrl', routes.runProbeJs);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
+var scriptprobe = require('scriptprobe');
 
-/*
- * GET home page.
- */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var remoteServer = 'scriptprobe-server-web.herokuapp.com';
+
+exports.index = function(req, res) {
+  res.render('index', { title: 'ScriptProbe', results: null, url: null, debug: null });
 };
+
+exports.runProbeHtml = function(req, res) {
+	scriptprobe.testUrl(req.body.url, remoteServer, function(results) {
+		res.render('index', { title: 'ScriptProbe results', 
+			results: results, url: req.body.url,
+			debug: JSON.stringify(results) });
+	});
+}
+
+exports.runProbeJs = function(req, res) {
+	scriptprobe.testUrl(req.query.url, remoteServer, function(results) {
+		res.render('json', { json: JSON.stringify(results) });
+	});
+}
